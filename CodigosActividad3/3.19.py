42783f5d11c10574755d1ebe80c058fdb9b51594
Gokdan <dan.04.cor@gmail.com>
2024-07-29 17:47:41 -0500

    Update 3.19.py

--- CodigosActividad3/3.19.py
+++ CodigosActividad3/3.19.py
@@ -2,28 +2,29 @@ import tkinter as tk
 from math import sqrt
 class Main:
     @staticmethod
-    def perimetroTriangulo(lado):
+    def perimetroTriangulo(lado): #Metodo que retorna el perimetro dado el argumento lado
         return lado*3
     
     @staticmethod
-    def alturaTriangulo(lado):
+    def alturaTriangulo(lado): #Metodo que retorna la altura dado el argumento lado
         return (sqrt(3)*lado)/2
     
 
     @staticmethod
-    def areaTriangulo(lado):
+    def areaTriangulo(lado): #Metodo que retorna el area dado el argumento lado
         return sqrt(3)*pow(lado,2)/4   
     
     @staticmethod
-    def info():
+    def info(): #Método que captura el valor de "lado" y lo emplea para el calculo 
         lado = float(entry_lado.get())
         perimetro = Main.perimetroTriangulo(lado)
         altura = Main.alturaTriangulo(lado)
         area = Main.areaTriangulo(lado)
         resultado.configure(text= f'Perimetro: {perimetro:.2f}unidades \n Area: {area:.2f} unidades cuadradas\n Altura {altura:.2f} unidades')
+#Creacion de la ventana
 ventana = tk.Tk()
 ventana.title('Triangulo')
-
+#Widgets
 label_lado = tk.Label(ventana,text='Ingrese la longitud del lado del triangulo equilatero: ')
 label_lado.pack()
 entry_lado = tk.Entry(ventana)
